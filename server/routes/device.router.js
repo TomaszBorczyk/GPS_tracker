506d7daf5dbedc278ce69835e13964388f20a18a
Tomasz <tomasso.holy@gmail.com>
2017-10-11 16:27:35 +0200

    progress

---
+++ server/routes/device.router.js
@@ -0,0 +1,11 @@
+const DeviceController = require('../controllers/device.controller'),
+    router = require('express').Router();
+
+    router.post('updatelocation', DeviceController.updateLocation);
+    router.post('registerDevice', DeviceController.registerDevice);
+
+    module.exports = router;
+
+
+
+    
