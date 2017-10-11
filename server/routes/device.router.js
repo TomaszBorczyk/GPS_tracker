47024beec2936dde68d139fb1d3e1c7b73963d14
Tomasz <tomasso.holy@gmail.com>
2017-10-11 23:22:39 +0200

    fixed route slashes

--- server/routes/device.router.js
+++ server/routes/device.router.js
@@ -1,7 +1,7 @@
 const DeviceController = require('../controllers/device.controller'),
     router = require('express').Router();
 
-router.post('updatelocation', DeviceController.updateLocation);
-router.post('registerDevice', DeviceController.registerDevice);
+router.post('/updatelocation', DeviceController.updateLocation);
+router.post('/registerDevice', DeviceController.registerDevice);
 
 module.exports = router;
