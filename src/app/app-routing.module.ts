23dc9ec3bc09f0b17aa0119a8da361fd6da526c5
Tomasz <tomasso.holy@gmail.com>
2017-10-30 02:05:36 +0100

    Dashboard view, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { Injectable, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { DevicesComponent } from './dashboard/devices/devices.component';
+import { MapComponent } from './dashboard/map/map.component';
+import { OptionsComponent } from './dashboard/options/options.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
@@ -9,7 +12,14 @@ import { RegisterComponent } from './register/register.component';
 @NgModule({
   imports: [
     RouterModule.forRoot([
-      { path: 'dashboard', component: DashboardComponent},
+      { path: 'dashboard', component: DashboardComponent,
+        children: [
+          { path: '', redirectTo: 'map', pathMatch: 'full' },
+          { path: 'map', component: MapComponent },
+          { path: 'devices', component: DevicesComponent },
+          { path: 'options', component: OptionsComponent }
+        ]
+      },
       { path: 'login', component: LoginComponent},
       { path: 'register', component: RegisterComponent},
       { path: '', redirectTo: 'login', pathMatch: 'full' },
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,11 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  private items: Array<object>;
+
+  constructor() {
+    this.items = [
+      { name: 'Map', icon: 'my_location', link: 'map'},
+      { name: 'Devices', icon: 'settings_remote', link: 'devices'},
+      { name: 'Options', icon: 'settings', link: 'options'},
+    ];
+  }
 
   ngOnInit() {
   }
