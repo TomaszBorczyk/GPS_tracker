f962e9d023d5a603d1b1595795b88aa8642a4c9f
Tomasz <TomaszBorczyk@users.noreply.github.com>
2017-10-31 20:54:11 +0100

    Auth guards (#7)
    * - Updated package.json versions
    
    * - Added guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,10 +9,37 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 
+@Injectable()
+export class IsLoggedIn implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> | boolean {
+    const isLoggedIn: boolean = localStorage.getItem('user') != null;
+    if (!isLoggedIn) {
+      this.router.navigate(['/login']);
+    }
+    return isLoggedIn;
+  }
+}
+
+@Injectable()
+export class IsLoggedOut implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> | boolean {
+    const isLoggedOut: boolean = localStorage.getItem('user') == null;
+    if (!isLoggedOut) {
+      this.router.navigate(['/dashboard']);
+    }
+    return isLoggedOut;
+  }
+}
+
+
 @NgModule({
   imports: [
     RouterModule.forRoot([
-      { path: 'dashboard', component: DashboardComponent,
+      { path: 'dashboard', component: DashboardComponent, canActivate: [IsLoggedIn],
         children: [
           { path: '', redirectTo: 'map', pathMatch: 'full' },
           { path: 'map', component: MapComponent },
@@ -20,12 +47,13 @@ import { RegisterComponent } from './register/register.component';
           { path: 'options', component: OptionsComponent }
         ]
       },
-      { path: 'login', component: LoginComponent},
-      { path: 'register', component: RegisterComponent},
+      { path: 'login', component: LoginComponent, canActivate: [IsLoggedOut]},
+      { path: 'register', component: RegisterComponent, canActivate: [IsLoggedOut]},
       { path: '', redirectTo: 'login', pathMatch: 'full' },
       { path: '**', redirectTo: 'login', pathMatch: 'full' }
     ])
   ],
+  providers: [IsLoggedIn, IsLoggedOut],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
