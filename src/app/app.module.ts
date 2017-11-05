a6140d5a40b066f34e22641409d16c61e09fb505
Tomasz <TomaszBorczyk@users.noreply.github.com>
2017-11-05 14:48:56 +0100

    Map (#10)
    * - Added map
    
    * - Displaying points and creating route
    
    * - Deleted unnecessary <p>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AgmCoreModule } from '@agm/core';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -6,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DevicesComponent } from './dashboard/devices/devices.component';
+import { DirectionsMapDirective } from './dashboard/map/map-direction.directive';
 import { MapComponent } from './dashboard/map/map.component';
 import { OptionsComponent } from './dashboard/options/options.component';
 import { AuthService } from './services/auth.service';
@@ -13,6 +15,7 @@ import { AlertService } from './welcome/alertService/alert.service';
 import { LoginComponent } from './welcome/login/login.component';
 import { RegisterComponent } from './welcome/register/register.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+
 
 @NgModule({
   declarations: [
@@ -23,14 +26,18 @@ import { WelcomeComponent } from './welcome/welcome.component';
     MapComponent,
     DevicesComponent,
     OptionsComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    DirectionsMapDirective
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
     FormsModule,
     HttpModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCJQ3YYQe4d37-LIG_84zwUQF4_Bior3ZA'
+    })
   ],
   providers: [
     AlertService,
--- src/app/dashboard/map/map.component.ts
+++ src/app/dashboard/map/map.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GPS } from '../../models/gps.model';
+import { default as mapMocks } from './map.mock';
 
 @Component({
   selector: 'app-map',
@@ -7,7 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  public datasets: Array<object>;
+  public positions: Array<GPS>;
+  public centerLocation: GPS;
+  public origin: GPS;
+  public destination: GPS;
+
+  constructor() {
+    this.datasets = [
+      { name: 'Mon, 14.10.2017'},
+      { name: 'Wen, 10.10.2017'},
+      { name: 'Fri, 06.10.2017'}
+    ];
+
+    this.positions = mapMocks.markers;
+    this.centerLocation = this.positions[this.positions.length - 1];
+    this.origin = this.positions[0];
+    this.destination = this.positions[1];
+  }
 
   ngOnInit() {
   }
